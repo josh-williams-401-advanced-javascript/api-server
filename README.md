ca29ac26357373966fb25d443da7e5500c741e14
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-08-19 22:02:28 -0700

    Update tests and README

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # API Server
 
+### 8/19/2020
+Requests now should be made on `/api/v1/products` and `/api/v1/categories`  
+POST, GET, PUT, and DELETE  
+
+Fill in a MongoDB port and Port in the .env  
+
+To test, run `npm test`  
+
+Note: This will not work if you try to go to a route with an invalid id  
+
+### 8/18/2020
+To use, run `node .`  
+
+Try making POST, GET, PUT, and DELETE requests on `/products` and `/categories`  
+
+See results sent or console-logged. 
+
 ### 8/17/2020
 To use, run the json server from the base:
 ```
@@ -16,9 +33,4 @@ http GET :3000/products
 
 Swagger: https://app.swaggerhub.com/apis/jswill88/api-server/0.1#/Model6_Array
 
-### 8/18/2020
-To use, run `node .`  
 
-Try making POST, GET, PUT, and DELETE requests on `/products` and `/categories`  
-
-See results sent or console-logged. 
--- __test__/server.test.js
+++ __test__/server.test.js
@@ -2,119 +2,167 @@
 
 const { server } = require('../lib/server');
 const supergoose = require('@code-fellows/supergoose');
-// const supertest = require('supertest');
 const mockRequest = supergoose(server);
 
-
-describe('Server - Products', () => {
-  // afterEach(async () => {
-  //   await mockRequest.delete('/api/v1/products/1');
-  // });
-
-  // it('Responds 404 on an invalid route', () => {
-
-  //   // const results = await mockRequest.get('/invalid');
-  //   // console.log(results.status);
-  //   // expect(results.status).toBe(404);
-  //   // return results;
-  //   return mockRequest
-  //     .get('/foo')
-  //     .then(results => {
-  //       expect(results.status).toBe(404);
-  //     });
-
-  // });
-
-  //   it('should respond properly to POST /products', async () => {
-  //     const results = await postProduct();
-  //     expect(results.body.name).toBe('shirt');
-  //     expect(results.status).toBe(201);
-  //   });
-
-  //   it('should respond properly to GET /products', async () => {
-  //     await postProduct();
-  //     const results = await mockRequest
-  //       .get('/products');
-  //     expect(results.status).toBe(200);
-  //     expect(results.body[0].name).toBe('shirt');
-  //   });
-
-  //   it('should respond to GET /products/:id', async () => {
-  //     await postProduct();
-  //     await postSecondProduct();
-  //     const results = await mockRequest
-  //       .get('/products/2');
-  //     expect(results.body.name).toBe('pants');
-  //     await mockRequest.delete('/products/2');
-  //   });
-
-  //   it('should respond to PUT /products/:id', async () => {
-  //     await postProduct();
-  //     await postSecondProduct();
-  //     const results = await mockRequest
-  //       .put('/products/2')
-  //       .send({
-  //         id: '2',
-  //         category: 'clothes',
-  //         name: 'tank top',
-  //         display_name: 'tank top',
-  //         description: 'No sleeves',
-  //       });
-  //     expect(results.body.name).toBe('tank top');
-  //     expect(results.status).toBe(201);
-  //     await mockRequest.delete('/products/2');
-  //   });
-
-  //   it('should respond to DELETE /products/:id', async() => {
-  //     await postProduct();
-  //     await postSecondProduct();
-  //     const results = await mockRequest.get('/products');
-  //     expect(results.status).toBe(200);
-  //     await mockRequest.delete('/products/2');
-  //     const newResults = await mockRequest.get('/products');
-  //     expect(newResults.body.length).toBe(1);
-  //   });
+let spy;
+beforeEach(() => {
+  spy = jest.spyOn(console, 'log').mockImplementation();
+});
+afterEach(()=> {
+  spy.mockRestore();
 });
 
-// const postProduct = async () => {
-//   return await mockRequest
-//     .post('/products')
-//     .send({
-//       id: '1',
-//       category: 'clothes',
-//       name: 'shirt',
-//       display_name: 'Shirt',
-//       description: 'two sleeves',
-//     });
-// };
-// const postSecondProduct = async () => {
-//   await mockRequest
-//     .post('/api/v1/products')
-//     .send({
-//       category: 'clothes',
-//       name: 'pants',
-//       display_name: 'Pants',
-//       description: 'Five pockets',
-// });
-// };
+describe('Server - Products', () => {
 
-// describe('Server - Categories', () => {
-//   afterEach(async () => {
-//     await mockRequest.delete('/categories/1');
-//   });
+  it('Responds 404 on an invalid route', async () => {
 
-//   it('should respond properly to POST /categories', async () => {
-//     const results = await postCat();
-//     expect(results.body.name).toBe('clothes');
-//     // expect(results.status).toBe(201);
-//   });
-// });
-// const postCat = async () => {
-//   return await mockRequest
-//     .post('/api/v1/categories')
-//     .send({
-//       name: 'clothes',
-//       display_name: 'Clothes',
-//       description: 'Body Covering',
-//     });
-// };
+    const results = await mockRequest.get('/invalid');
+    expect(results.status).toBe(404);
+    return results;
+  });
+
+  it('should respond properly to POST /products', async () => {
+    const results = await postProduct();
+    expect(results.body.name).toBe('shirt');
+    expect(results.status).toBe(201);
+  });
+
+  it('should respond properly to GET /products', async () => {
+    await postProduct();
+    const results = await mockRequest
+      .get('/api/v1/products');
+    expect(results.status).toBe(200);
+    expect(results.body[0].name).toBe('shirt');
+  });
+
+  it('should respond to GET /products/:id', async () => {
+    await postProduct();
+    let secondProduct = await postSecondProduct();
+    let id = secondProduct.body._id;
+    const results = await mockRequest
+      .get(`/api/v1/products/${id}`);
+    expect(results.body.name).toBe('pants');
+  });
+
+  it('should respond to PUT /products/:id', async () => {
+    await postProduct();
+    let secondProduct = await postSecondProduct();
+    let id = secondProduct.body._id;
+    const results = await mockRequest
+      .put(`/api/v1/products/${id}`)
+      .send({
+        category: 'clothes',
+        name: 'tank top',
+        display_name: 'tank top',
+        description: 'No sleeves',
+      });
+    expect(results.body.name).toBe('tank top');
+    expect(results.status).toBe(201);
+  });
+
+
+  it('should respond to DELETE /products/:id', async () => {
+    await postProduct();
+    let secondProduct = await postSecondProduct();
+    let id = secondProduct.body._id;
+    const results = await mockRequest.get('/api/v1/products');
+    let lengthOne = results.body.length;
+    expect(results.status).toBe(200);
+    await mockRequest.delete(`/api/v1/products/${id}`);
+    const newResults = await mockRequest.get('/api/v1/products');
+    expect(newResults.body.length < lengthOne).toBe(true);
+  });
+});
+
+const postProduct = async () => {
+  return await mockRequest
+    .post('/api/v1/products')
+    .send({
+      category: 'clothes',
+      name: 'shirt',
+      display_name: 'Shirt',
+      description: 'two sleeves',
+    });
+};
+const postSecondProduct = async () => {
+  return await mockRequest
+    .post('/api/v1/products')
+    .send({
+      category: 'clothes',
+      name: 'pants',
+      display_name: 'Pants',
+      description: 'Five pockets',
+    });
+};
+
+describe('Server - Categories', () => {
+  it('should respond properly to POST /categories', async () => {
+    const results = await postCat();
+    expect(results.body.name).toBe('clothes');
+    expect(results.status).toBe(201);
+  });
+
+  it('should respond properly to GET /categories', async () => {
+    await postCat();
+    const results = await mockRequest
+      .get('/api/v1/categories');
+    expect(results.status).toBe(200);
+    expect(results.body[0].name).toBe('clothes');
+  });
+
+  it('should respond to GET /categories/:id', async () => {
+    await postCat();
+    let secondCat = await postSecondCat();
+    let id = secondCat.body._id;
+    const results = await mockRequest
+      .get(`/api/v1/categories/${id}`);
+    expect(results.body.name).toBe('instruments');
+  });
+
+  it('should respond to PUT /categories/:id', async () => {
+    await postCat();
+    let secondCat = await postSecondCat();
+    let id = secondCat.body._id;
+    const results = await mockRequest
+      .put(`/api/v1/categories/${id}`)
+      .send({
+        name: 'cars',
+        display_name: 'Cars',
+        description: 'Go Go Mobiles',
+      });
+    expect(results.body.name).toBe('cars');
+    expect(results.status).toBe(201);
+  });
+
+  it('should respond to DELETE /categories/:id', async () => {
+    await postCat();
+    let secondCat = await postSecondCat();
+    let id = secondCat.body._id;
+    const results = await mockRequest.get('/api/v1/categories');
+    let lengthOne = results.body.length;
+    expect(results.status).toBe(200);
+    await mockRequest.delete(`/api/v1/categories/${id}`);
+    const newResults = await mockRequest.get('/api/v1/categories');
+    expect(newResults.body.length < lengthOne).toBe(true);
+  });
+
+});
+
+const postCat = async () => {
+  return await mockRequest
+    .post('/api/v1/categories')
+    .send({
+      name: 'clothes',
+      display_name: 'Clothes',
+      description: 'Body Covering',
+    });
+};
+const postSecondCat = async () => {
+  return await mockRequest
+    .post('/api/v1/categories')
+    .send({
+      name: 'instruments',
+      display_name: 'Instruments',
+      description: 'Sound Machines',
+    });
+};
